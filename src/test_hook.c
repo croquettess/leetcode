19a9ff9a7b6f2ed87e49dcb9969b3be6f16acf3f
croquetess <sun.ruiyang@outlook.com>
2025-01-14 15:08:03 +0800

    feat [JIRA-1234] : hook

---
+++ src/test_hook.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+int main() {
+    void *ptr1 = malloc(4);
+    void *ptr2 = malloc(4 * 1024);
+    void *ptr3 = malloc(4 * 1024 * 1024);
+    void *ptr4 = malloc(4);
+    void *ptr5 = malloc(4 * 1024);
+    void *ptr6 = malloc(4 * 1024 * 1024);
+    void *ptr7 = malloc(4);
+    void *ptr8 = malloc(4 * 1024);
+    void *ptr9 = malloc(4 * 1024 * 1024);
+    free(ptr1);
+    free(ptr2);
+    free(ptr3);
+    free(ptr4);
+    free(ptr5);
+    free(ptr6);
+    free(ptr7);
+    free(ptr8);
+    free(ptr9);
+    return 0;
+}
--- src/x.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-  cout << "test webhook" << endl;
-  return 0;
-}
